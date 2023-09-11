cf619f4aeea0a6af01cacbf4a030ed065d277d42
Erwin Dupont <erwin.dupont@acagroup.be>
2023-09-11 22:30:25 +0200

    [boardgame-insights-3] WIP User Status

--- client/src/components/Header/Status/Status.tsx
+++ client/src/components/Header/Status/Status.tsx
@@ -1,23 +1,44 @@
-import {AnonymousUser, LoggedInUser} from "./User";
+import {AnonymousUser, LoggedInUser} from './User';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export interface IBggUser {
+    id?: string;
+    username?: string;
+    email?: string;
+}
 
 export const Status = () => {
-    // fetch with GET
-    // 	https://boardgamegeek.com/api/accounts/current
-    // await fetch("https://boardgamegeek.com/api/accounts/current", {
-    //     "credentials": "include",
-    //     "headers": {
-    //         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0",
-    //         "Accept": "application/json, text/plain, */*",
-    //         "Accept-Language": "en-US,en;q=0.5"
-    //     },
-    //     "referrer": "https://boardgamegeek.com/",
-    //     "method": "GET",
-    //     "mode": "cors"
-    // });
+    const [bggUser, setBggUser] = useState({} as IBggUser);
+
+    useEffect(() => {
+        axios.get('https://boardgamegeek.com/api/accounts/current', {
+            // mode: 'no-cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            withCredentials: true,
+            // credentials: 'same-origin',
+        })
+            .then(response => {
+                setBggUser({
+                    id: response.data.user,
+                    username: response.data.username,
+                    email: response.data.email
+                })
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                setBggUser({} as IBggUser)
+            });
+    }, []);
+
     return (
         <>
-            <LoggedInUser username="ForumMortsel"/>
-            <AnonymousUser/>
+            {bggUser.username && <LoggedInUser username={bggUser.username}/>}
+            {!bggUser.username && <AnonymousUser/>}
         </>
     );
 }
